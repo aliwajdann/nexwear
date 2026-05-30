1f16056c87745e96bfcdda0ce3b1ec5f9eb65b1f
Ali Wajdan <aliwajdan.it@gmail.com>
2026-05-30 14:47:14 +0500

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    domains: ['cdn.velanoshop.store'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.velanoshop.store',
+      },
+    ],
   },
-
-  
 };
-
-export const config = {
-  matcher: ['/admin/:path*'], // 👈 Middleware runs ONLY on /admin routes
-};
-
 
 export default nextConfig;
